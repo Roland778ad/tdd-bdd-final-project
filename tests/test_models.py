458f2bf6a4d33bc7791a0e366b2ce8401a052bde
Roland <roland778ad@gmail.com>
2025-02-18 13:55:20 -0500

    Scheduled update, minimal changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+ # Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -104,3 +104,6 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    # Test Read function of application
+    def 
